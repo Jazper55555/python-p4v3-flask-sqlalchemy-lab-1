6560717bf7f15603a7aa3d3bbadfb91de808395c
Jazper <jazpersaldana@yahoo.com>
2024-07-25 19:11:07 -0500

    flask alchemy lab 1 complete

--- server/app.py
+++ server/app.py
@@ -21,6 +21,45 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    magnitude_list = []
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    for earthquake in earthquakes:
+        earthquake_body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        magnitude_list.append(earthquake_body)
+
+    body = {
+        'count': len(magnitude_list),
+        'quakes': magnitude_list
+    }
+
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
